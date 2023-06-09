d676d8e559b9020b72c26da8d5494d763fd927ca
Joyeeta Sarkar <joyeetasarkar1410@gmail.com>
2023-06-09 13:15:08 +0600

    Footer Addded

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,14 @@
 // import React from 'react';
 
 import { Outlet } from "react-router-dom";
+import Footer from "../SharedItems/Footer";
 
 const Main = () => {
     return (
         <div>
             Mainnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/SharedItems/Footer.jsx
@@ -0,0 +1,37 @@
+// import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer p-10 bg-black text-white">
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </div>
+            </footer>
+            <footer className="footer footer-center p-4 bg-black bg-opacity-90 text-white">
+                <div>
+                    <p>Copyright © 2023 - All right reserved by ACOUSTICA</p>
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
